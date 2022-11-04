9410c737c31add1184bcb38a39d270b7d84d6519
EDockhorn <johnnyerick111@hotmail.com>
2022-11-03 23:16:30 -0300

    Create a MVC basic application for simulate a post transfer method.

---
+++ Presentation/MicroRabbit.MVC/MicroRabbit.MVC/Services/TransferService.cs
@@ -0,0 +1,35 @@
+﻿using MicroRabbitMQ.MVC.Models.DTO;
+using Newtonsoft.Json;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace MicroRabbitMQ.MVC.Services
+{
+    public interface ITransferService
+    {
+        Task Transfer(TransferDto transferDto);
+    }
+
+    public class TransferService : ITransferService
+    {
+        private readonly HttpClient _apiClient;
+
+        public TransferService(HttpClient apiClient)
+        {
+            _apiClient = apiClient;
+        }
+
+        public async Task Transfer(TransferDto transferDto)
+        {
+            var uri = "https://localhost:5001/api/Banking";
+            var transferContent = new StringContent(
+                JsonConvert.SerializeObject(transferDto),
+                System.Text.Encoding.UTF8, "application/json");
+
+            var response = await _apiClient.PostAsync(uri, transferContent);
+            response.EnsureSuccessStatusCode();
+
+
+        }
+    }
+}
---
+++ Presentation/MicroRabbitMQ.MVC/MicroRabbitMQ.MVC/Controllers/HomeController.cs
@@ -0,0 +1,41 @@
+﻿using MicroRabbitMQ.MVC.Models;
+using MicroRabbitMQ.MVC.Models.DTO;
+using MicroRabbitMQ.MVC.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace MicroRabbitMQ.MVC.Controllers
+{
+    public class HomeController : Controller
+    {
+
+        private readonly ITransferService _transferService;
+
+        public HomeController(ITransferService transferService)
+        {
+            _transferService = transferService;
+        }
+
+        // GET: HomeController
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Transfer(TransferViewModel model)
+        {
+            TransferDto transferDto = new()
+            {
+                FromAccount = model.FromAccount,
+                ToAccount = model.ToAccount,
+                TransferAmount = model.TransferAmount
+            };
+
+            await _transferService.Transfer(transferDto);
+
+            return View("Index");
+        }
+    }
+}
